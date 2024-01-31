61496d8d9051cb857452ee524d6cec2d7f8dd4b2
unknown <srikanthcollaty92@gmail.com>
2024-01-31 15:40:20 +0100

    Updates

--- controller/ProfileObjectPageHeader-dbg.controller.js
+++ controller/ProfileObjectPageHeader-dbg.controller.js
@@ -8,6 +8,22 @@ sap.ui.define([
 		onInit: function () {
 			var oJsonModel = new JSONModel(sap.ui.require.toUrl("sap/uxap/sample/SharedJSONData/employee.json"));
 			this.getView().setModel(oJsonModel, "ObjectPageModel");
-		}
+		},
+
+		handlePressLinkedIn        : function (oEvent) {
+			window.location.href = "https://www.linkedin.com/";
+		},
+		handlePressGitHub: function (oEvent) {
+			window.location.href= "https://github.com/srikanthsc";
+		},
+		handlePressYoutube: function (oEvent) {
+			window.location.href="https://www.youtube.com/@CinematicSpectra-fn1qd";
+		},
+		handlePressGmail: function (oEvent) {
+			window.location.href="https://www.gmail.com/";
+		},
+		handlePressTwitter: function (oEvent) {
+			window.location.href="https://www.twitter.com/";
+		},
 	});
 });
